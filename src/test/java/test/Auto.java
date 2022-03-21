08ea4f8e796386c81774bbc5ba05168768f2a7a6
GennerPineda <gepinedace@unal.edu.co>
2022-03-21 12:02:28 -0500

    clases requeridas
    Añadi la clase Asiento, Aut y Motor

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,50 @@
+package test;
+
+public class Auto {
+	
+		
+		String modelo;
+		int precio;
+		Asiento[] asientos;
+		String marca;
+		Motor motor;
+		int registro;
+		static int cantidadCreados;
+		
+	    int cantidadAsientos() {
+	    	 int n = 0;
+	    	 for(int i = 0; i < asientos.length; i++) {
+	    		 if (asientos[i] != null) {
+	    			 n += 1;
+	    		 }
+	    	 }
+	    	 return n;
+	    }
+		
+		
+	    String verificarIntegridad() {
+			
+	    	if (motor.registro == registro) {
+	    		
+	    		for(int i = 0; i < asientos.length;i++ ) {
+	    			if (asientos[i] == null) {
+	    				continue;
+	    			}
+	    			if (asientos[i].registro != registro) {
+	    				return "Las piezas no son originales";
+	    			}	
+	    		}
+	    		return "Auto original";
+	    		
+	    	}
+	    	else {
+	    		return "Las piezas no son originales";
+	    	}
+	    	
+			
+		}
+		
+	}
+	
+
+
